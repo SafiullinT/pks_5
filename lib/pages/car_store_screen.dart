28dd1d8a09dee1193e3c48af3ba27075424965a7
Timur <64526441+SafiullinT@users.noreply.github.com>
2024-10-08 04:36:51 +0300

    Initial commit

--- lib/pages/car_store_screen.dart
+++ lib/pages/car_store_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../models/car.dart';
 import 'add_car_screen.dart';
 import '../components/car_card.dart';
-import 'car_detail_screen.dart'; // Импортируйте экран деталей автомобиля
+import 'car_detail_screen.dart';
 
 class CarStoreScreen extends StatefulWidget {
   final List<Car> cars;
@@ -18,7 +18,7 @@ class CarStoreScreen extends StatefulWidget {
 class _CarStoreScreenState extends State<CarStoreScreen> {
   void _deleteCar(Car car) {
     setState(() {
-      widget.cars.remove(car); // Удаляем машину из списка
+      widget.cars.remove(car);
     });
   }
 
@@ -32,26 +32,25 @@ class _CarStoreScreenState extends State<CarStoreScreen> {
           ? Center(child: Text('Нет автомобилей в магазине'))
           : GridView.builder(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // Количество столбцов
-          childAspectRatio: 0.8, // Соотношение сторон карточек
-          crossAxisSpacing: 10, // Пробел между столбцами
-          mainAxisSpacing: 10, // Пробел между строками
+          crossAxisCount: 2,
+          childAspectRatio: 0.8,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
         ),
         itemCount: widget.cars.length,
         itemBuilder: (context, index) {
           final car = widget.cars[index];
           return CarCard(
             car: car,
-            onDeleteCar: () => _deleteCar(car), // Передаем функцию удаления
+            onDeleteCar: () => _deleteCar(car),
             onToggleFavorite: () => widget.toggleFavorite(car),
-            // Удаляем нажатие на карточку, добавляем переход на экран деталей
             onTap: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) => CarDetailScreen(
                     car: car,
-                    onDeleteCar: () => _deleteCar(car), // Передаем функцию удаления
+                    onDeleteCar: () => _deleteCar(car),
                   ),
                 ),
               );
